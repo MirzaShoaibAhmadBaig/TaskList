9548c9939f0f9cb05f6194b0a3ac9d0392736eab
armanahmd <92032906+armanahmd@users.noreply.github.com>
2022-03-20 17:03:03 +0500

    First Commit SetUp DB Context

---
+++ TaskList.DAL/TaskListDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskList.DAL
+{
+    public class TaskListDBContext : DbContext
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public TaskListDBContext(DbContextOptions<TaskListDBContext> con, IHttpContextAccessor httpContextAccessor) :base(con) 
+        {
+            _httpContextAccessor = httpContextAccessor;
+
+        }
+
+    }
+}
